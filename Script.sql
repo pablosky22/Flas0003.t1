52fcba825bfc4f2beb7c3a5b60a85d248f4e400d
pablo <pablo@192.168.1.162>
2018-05-23 15:36:17 -0300

    Proyecto web con validación de datos Version 000002

---
+++ Script.sql
@@ -0,0 +1,19 @@
+﻿create table categoria(
+id int primary key not null auto_increment,
+nombre varchar(45) not null unique, 
+descripcion varchar(150) null,
+fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table producto(
+id int primary key auto_increment,
+nombre varchar(100) not null unique,
+cantidad int null default 0,
+valor double null default 0,
+fkCategoria int not null);
+
+alter table producto add constraint
+PadreCategoria foreign key (fkCategoria)
+references categoria(id) on delete restrict on 
+update cascade;
+ 
